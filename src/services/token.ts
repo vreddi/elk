5246e4573902325c90a67c91442379ff579cd35a
Vishrut Reddi <vishrutreddi@gmail.com>
2020-04-29 00:39:06 -0700

    Progress

--- src/services/token.ts
+++ src/services/token.ts
@@ -11,6 +11,7 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET as string);
     (req as any).user = verified;
+    next();
   } catch (error) {
     res.status(400).send("Invalid token");
   }
